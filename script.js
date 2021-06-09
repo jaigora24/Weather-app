7d678cac107ebde4ffdc9164e618603ea6c040f0
Jai Gora <Jaigora24@gmail.com>
2021-06-10 00:54:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,10 @@ $(document).ready(function(){
             url : 'http://api.openweathermap.org/data/2.5/weather',
             dataType: 'json',
             type: 'GET',
-            data: {q:city, appid: key, units: 'metric'},
+            data: {
+                   q:city,
+                   appid: key,
+                   units: 'metric'},
 
             success: function(data){
 
@@ -18,6 +21,7 @@ $(document).ready(function(){
                 
                 $.each(data.weather, function(index, val){
                     wf += '<h1><b>'+data.name+"</b><h1>" +"<h3>Temperature&nbsp;:&nbsp;"+ data.main.temp + '&deg;C </h3>' /*+ val.main + ","*/+"<h3>Description&nbsp;:&nbsp;" + val.description+ '</h3>'
+                
                 });
 
                 $("#showWeatherForecast").html(wf);
